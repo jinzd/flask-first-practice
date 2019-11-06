9b68ef3e0e2791aeba4705cc1def53b626960b9e
andy <andytan727@gmail.com>
2019-11-06 16:17:45 +0800

    initial commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return '<h1>Why so easy</h1>'
+
+
+# @app.route("/another")
+# def show():
+#     return '<h1>Yo</h1>'
+
+
+@app.route("/user/<username>")
+def show(username):
+    return f"Hi {username[0:5]}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
